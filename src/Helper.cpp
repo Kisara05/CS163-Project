848c1dd1b7c96bf201d6fd0cf951e011d0991e5e
nabhcmus <nabsubyt1@gmail.com>
2024-08-08 22:26:29 +0700

    Create the helper file

---
+++ src/Helper.cpp
@@ -0,0 +1,13 @@
+#include <Helper.h>
+
+std::string convertToNonAccentVN(std::string str) {
+
+}
+
+std::string normalize(std::string str) {
+
+}
+
+std::vector<std::string> split(const std::string &str, char delimiter) {
+
+}
--- src/Helper.h
+++ src/Helper.h
@@ -1,2 +1,14 @@
+#ifndef HELPER_H
 #define HELPER.H
-using namespace std;
+
+#include <string>
+#include <vector>
+#include <iostream>
+
+std::string convertToNonAccentVN(std::string str);
+
+std::string normalize(std::string str);
+
+std::vector<std::string> split(const std::string& str, char delimiter);
+
+#endif //HELPER_H
