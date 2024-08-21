13b83787ff850bc6858c3378a0575c447c26b369
Dtaz125 <dangthanhdat125@gmail.com>
2024-08-21 20:13:54 +0700

    Update StateStack.h

--- src/StateStack.h
+++ src/StateStack.h
@@ -1,8 +1,14 @@
 #ifndef STATESTACK_H
 #define STATESTACK_H
+#include "StateStack.inl"
 
 #include "States/State.h"
 
-
-
+class StateStack {
+public:
+    explicit StateStack(State::Context context);
+    bool isEmpty() const;
+   
+    bool isEmpty() const;
+};
 #endif
