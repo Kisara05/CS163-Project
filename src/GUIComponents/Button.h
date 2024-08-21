3cd90bb3c0db191d23d9e159c6d1b0d557431a18
nabhcmus <nabsubyt1@gmail.com>
2024-08-21 22:48:31 +0700

    add some component of button's

--- src/GUIComponents/Button.h
+++ src/GUIComponents/Button.h
@@ -1,4 +1,24 @@
 #ifndef BUTTON_H
 #define BUTTON_H
+#include "GUIComponent.h"
+#include "raylib.h"
+#include <functional>
+#include <memory>
+#include <string>
+
+class Button : public GUIComponent {
+public:
+    typedef std::shared_ptr<Button> SmartPointer;
+
+    typedef std::function<Color(Color)> Filter;
+
+    typedef std::function<void()> Callback;
+
+    enum class ButtonState {
+        None,
+        Focused,
+        Active
+    };
+};
 
 #endif // BUTTON_H
