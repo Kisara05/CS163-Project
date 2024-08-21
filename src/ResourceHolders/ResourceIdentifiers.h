4bbd611617a8e4d7a77783ed28351acc24c1c755
BlueAngel05 <hoangdonthienhoa@gmail.com>
2024-08-21 20:31:59 +0700

    add GUI+

---
+++ src/ResourceHolders/ResourceIdentifiers.h
@@ -0,0 +1,33 @@
+#ifndef RESOURCEHOLDERS_RESOURCEIDENTIFIERS_H
+#define RESOURCEHOLDERS_RESOURCEIDENTIFIERS_H
+
+enum class TextureID {
+    None,
+    DefinitionSwitch,
+    KeywordSwitch,
+    Return,
+    Edit,
+    Cancel,
+    Save,
+    AddDefinition,
+    DatasetSwitch,
+    SearchNav,
+    FavoriteNav,
+    GameNav,
+    OptionPanel,
+    StarUnselected,
+    StarSelected,
+    Settings,
+    Delete,
+    Details,
+    WordGame,
+    DefinitionGame
+};
+
+enum class FontID {
+    None,
+    Inter,
+    Inter_Bold,
+};
+
+#endif // RESOURCEHOLDERS_RESOURCEIDENTIFIERS_H
