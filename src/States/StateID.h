d7a44197d70e0c99aad84710dd88bbb360fd1609
BlueAngel05 <hoangdonthienhoa@gmail.com>
2024-08-28 14:32:33 +0700

    add

---
+++ src/States/StateID.h
@@ -0,0 +1,14 @@
+#ifndef STATES_STATEIDENTIFIERS_H
+#define STATES_STATEIDENTIFIERS_H
+
+enum class StateIDs {
+    None,
+    Search,
+    Favorite,
+    Game,
+    WordGame,
+    DefinitionGame,
+    Settings
+};
+
+#endif // STATES_STATEIDENTIFIERS_H
