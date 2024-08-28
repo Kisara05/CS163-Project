20a52c4d93c518494fd521f2ca629088ba54d0de
nabhcmus <nabsubyt1@gmail.com>
2024-08-28 21:58:31 +0700

    Update State.h

--- src/States/State.h
+++ src/States/State.h
@@ -1,11 +1,28 @@
+#include "../ColorPalettes/AppColorPalette.h"
+#include "../ResourceHolders/TextureHolder.h"
+#include "StateID.h"
+#include "../NavBar.h"
+
 #include "memory.h"
 
 class StateStack;
 
 class State {
 public:
+    typedef std::unique_ptr<State> Ptr;
 	struct Context {};
-    
+    State(StateStack &stack, Context context);
+    virtual ~State();
+
+    virtual bool update(float dt) = 0;
+    virtual void draw() = 0;
+
+protected:
+    NavBar mNavBar;
+    void requestStackPush(StateIDs stateID);
+    void requestStackPop();
+    void requestStackClear();
+
 private:
     StateStack* stack;
     Context context;
