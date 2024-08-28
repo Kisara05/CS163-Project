575092225de9e00a6b70a3bef6d8cf51c08a9f21
BlueAngel05 <hoangdonthienhoa@gmail.com>
2024-08-28 14:23:13 +0700

    add

---
+++ src/States/SearchState.h
@@ -0,0 +1,34 @@
+#ifndef STATES_SEARCHSTATE_H
+#define STATES_SEARCHSTATE_H
+
+#include "../Core/Dictionary.h"
+#include "../GUIComponents/TextBox.h"
+#include "../GUIComponents/InputBox.h"
+#include "../GUIComponents/Button.h"
+#include "../WordList.h"
+#include "State.h"
+
+#include <map>
+
+class SearchState : public State {
+public:
+    SearchState(StateStack& stack, Context context);
+    ~SearchState();
+
+    bool update(float dt);
+    void draw();
+
+private:
+    std::vector<Core::Word*> mWordList;
+
+    bool mIsDefinitionSearch;
+
+private: // UI elements
+    InputBox mSearchBar;
+    Button mSearchButton;
+    WordList mScrollList;
+
+    Button mModeButton;
+};
+
+#endif // STATES_SEARCHSTATE_H
